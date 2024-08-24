ef74ac4d7d7ba8a6c229a9172140cc50687642a6
Alexisn97 <niyonkurua97@gmail.com>
2024-08-24 17:44:07 +0200

    Product Delete and Category Delete and Update

--- src/helpers/uploadImages.js
+++ src/helpers/uploadImages.js
@@ -5,9 +5,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLAUDINARY_NAME,
+  api_key: process.env.CLAUDINARY_API_KEY,
+  api_secret: process.env.CLAUDINARY_API_SECRET,
 });
 
 export const uploadImage = async (fileToUpload) => {
--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -3,7 +3,10 @@ import upload from "../helpers/multer.js";
 import { bodyValidation, transformFilesToBody, isIdValid } from '../middlewares/validations.js';
 import {
     createProductsSchema,
-    createCategorySchema
+    createCategorySchema,
+    updateCategorySchema,
+    deleteCategorySchema,
+    deleteProductSchema
 } from '../modules/product/validations/productsValidations.js';
 import {
     isProductAlreadyExists,
@@ -17,12 +20,16 @@ import productController from '../modules/product/controller/productController.j
 import { isUserAuthorized } from '../middlewares/userAuthorization.js';
 const router = express.Router();
 
-router.post("/create-product",isUserAuthorized(['admin']),  upload.array("images"), transformFilesToBody, bodyValidation(createProductsSchema), isProductAlreadyExists,isCategoryExistByCategory, productController.adminCreateProduct)
+router.post("/create-product",isUserAuthorized(['admin']),  upload.array('images'), transformFilesToBody, bodyValidation(createProductsSchema), isProductAlreadyExists,isCategoryExistByCategory, productController.adminCreateProduct)
 router.get("/view-all-products", isProductsExists, productController.getAllProducts)
 router.get("/view-specific-product/:id", isIdValid, isProductExists, productController.getSingleProduct)
+router.delete("/delete-product", isUserAuthorized(['admin']), isProductExists, bodyValidation(deleteProductSchema),productController.adminDeleteProduct)
 
 
 router.post("/create-category", isUserAuthorized(['admin']), bodyValidation(createCategorySchema), isCategoryAlreadyExists, productController.adminCreateCategory)
 router.get("/view-categories", isCategoriesExists, productController.adminViewCategories)
+
+router.put("/update-category", isUserAuthorized(['admin']), isCategoriesExists, bodyValidation(updateCategorySchema), productController.adminUpdateCategory)
+router.delete("/Delete-category", isUserAuthorized(['admin']), isCategoriesExists, bodyValidation(deleteCategorySchema), productController.adminDeleteCategory)
 
 export default router
