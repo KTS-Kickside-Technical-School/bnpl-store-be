90a8657899cdcf2add96692516b239c3d3f7d16c
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-09-20 02:08:45 +0200

    SOlve the issues mainly focusing to cart: i  added the view cartItems endpoint and made some things online

--- src/modules/cart/controller/cartController.js
+++ src/modules/cart/controller/cartController.js
@@ -2,45 +2,46 @@ import httpStatus from "http-status";
 import cartRepository from "../repository/cartRepository.js";
 
 
-export const addProductToCart = async(req, res)=>{
+
+const addProductToCart = async (req, res) => {
     try {
 
         const { user, product } = req;
         if (!user || !user._id || !product || !product._id) {
-          return res.status(httpStatus.BAD_REQUEST).json({
-            status: httpStatus.BAD_REQUEST,
-            message: "User or product information is missing",
-          });
+            return res.status(httpStatus.BAD_REQUEST).json({
+                status: httpStatus.BAD_REQUEST,
+                message: "User or product information is missing",
+            });
         }
 
         const userId = req.user._id;
         const productId = req.product._id;
         const quantity = req.body.quantity;
-        const data = {userId, productId, quantity}
-        
-        const cart= await cartRepository.addProductToCart(data);
+        const data = { userId, productId, quantity }
+
+        const cart = await cartRepository.addProductToCart(data);
         return res.status(httpStatus.CREATED).json({
             status: httpStatus.CREATED,
             message: "Product added Successfully to Cart",
-            data: {cart}
+            data: { cart }
         })
-        
+
     } catch (error) {
         return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
             status: httpStatus.INTERNAL_SERVER_ERROR,
             message: "Error adding product to cart",
         });
-        
+
     }
 };
 
 
-export const removeProductFromCart = async (req, res)=>{
+const removeProductFromCart = async (req, res) => {
     try {
         const productId = req.body.productId
-        const removeCartItems= await cartRepository.deleteProductFromCart(productId)
+        const removeCartItems = await cartRepository.deleteProductFromCart(productId)
 
-        if (!removeCartItems){
+        if (!removeCartItems) {
             return res.status(httpStatus.NOT_FOUND).json({
                 status: httpStatus.NOT_FOUND,
                 message: "Product not found in cart",
@@ -52,20 +53,33 @@ export const removeProductFromCart = async (req, res)=>{
         })
 
 
-        
+
     } catch (error) {
         return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
             status: httpStatus.INTERNAL_SERVER_ERROR,
             message: message.error
         })
-        
+
     }
 };
 
+const getCartItems = (req, res) => {
+    try {
+        return res.status(httpStatus.OK).json({
+            status: httpStatus.OK,
+            message: "Cart Items retrieved successfully",
+            data: { cartItems: req.cartItems }
+        });
+    } catch (error) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            status: httpStatus.INTERNAL_SERVER_ERROR,
+            messager: error.message || "Something went wrong"
+        })
+    }
+}
 
-
-export default{
+export default {
     addProductToCart,
-    removeProductFromCart
-
+    removeProductFromCart,
+    getCartItems
 }
